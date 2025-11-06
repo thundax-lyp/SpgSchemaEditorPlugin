3cfebc48b5bcf16778439b881f2c59bbf9aebf66
thundax-lyp <731378491@qq.com>
2025-11-06 10:18:09 +0800

    fixed: update label_property_list

--- idea/spg-schema-idea-plugin/src/main/java/org/openspg/idea/conceptRule/annotator/processor/ConceptRuleHighlightingProcessor.java
+++ idea/spg-schema-idea-plugin/src/main/java/org/openspg/idea/conceptRule/annotator/processor/ConceptRuleHighlightingProcessor.java
@@ -5,21 +5,26 @@ import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.openspg.idea.common.annotator.AnnotateProcessor;
+import org.openspg.idea.conceptRule.ConceptRuleBundle;
 import org.openspg.idea.conceptRule.highlighter.ConceptRuleHighlightingColors;
-import org.openspg.idea.conceptRule.lang.psi.ConceptRuleFunctionName;
-import org.openspg.idea.conceptRule.lang.psi.ConceptRuleLabelName;
-import org.openspg.idea.conceptRule.lang.psi.ConceptRuleRuleWrapperPattern;
+import org.openspg.idea.conceptRule.lang.psi.*;
+import org.openspg.idea.schema.SchemaBundle;
+
+import java.util.List;
 
 public class ConceptRuleHighlightingProcessor implements AnnotateProcessor {
 
+    @Override
     public boolean process(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
         TextAttributesKey textAttributesKey = null;
 
         if (element instanceof ConceptRuleLabelName
                 && PsiTreeUtil.getParentOfType(element, ConceptRuleRuleWrapperPattern.class) != null) {
             textAttributesKey = ConceptRuleHighlightingColors.WRAPPER_PATTERN;
+
         } else if (element instanceof ConceptRuleFunctionName) {
             textAttributesKey = ConceptRuleHighlightingColors.FUNCTION;
         }
@@ -31,6 +36,27 @@ public class ConceptRuleHighlightingProcessor implements AnnotateProcessor {
                     .create();
         }
 
+        if (element instanceof ConceptRuleLabelPropertyList) {
+            return this.checkLabelPropertyList((ConceptRuleLabelPropertyList) element, holder);
+        }
+
+        return true;
+    }
+
+    private boolean checkLabelPropertyList(@NotNull final ConceptRuleLabelPropertyList labelPropertyList, @NotNull AnnotationHolder holder) {
+        List<ConceptRuleLabelProperty> labelProperties = labelPropertyList.getLabelPropertyList();
+        for (int i = 1; i < labelProperties.size(); i++) {
+            ConceptRuleLabelProperty prevElement = labelProperties.get(i - 1);
+            ConceptRuleLabelProperty currElement = labelProperties.get(i);
+            // label-name should be before property-expression
+            if (currElement.getLabelName() != null && prevElement.getPropertyExpression() != null) {
+                holder.newAnnotation(HighlightSeverity.ERROR, currElement.getText())
+                        .range(currElement.getTextRange())
+                        .tooltip(ConceptRuleBundle.message("ConceptRuleAnnotator.error.badLabelPropertyExpressionOrder"))
+                        .create();
+                return false;
+            }
+        }
         return true;
     }
 
