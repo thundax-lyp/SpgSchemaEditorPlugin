779a044cabeb3393519ff3ac16b8a9c0878e0d01
thundax-lyp <731378491@qq.com>
2025-12-26 11:11:26 +0800

    0.0.17-dev

--- idea/spg-schema-idea-plugin/src/main/java/org/openspg/idea/schema/structureView/viewElement/SchemaEntityStructureViewElement.java
+++ idea/spg-schema-idea-plugin/src/main/java/org/openspg/idea/schema/structureView/viewElement/SchemaEntityStructureViewElement.java
@@ -2,16 +2,18 @@ package org.openspg.idea.schema.structureView.viewElement;
 
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.util.treeView.smartTree.TreeElement;
-import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.openspg.idea.schema.SchemaIcons;
+import org.openspg.idea.schema.psi.SchemaBasicStructureDeclaration;
 import org.openspg.idea.schema.psi.SchemaEntity;
-import org.openspg.idea.schema.psi.SchemaEntityHead;
-import org.openspg.idea.schema.psi.SchemaProperty;
 
-import java.util.List;
+import javax.swing.*;
 
 public class SchemaEntityStructureViewElement extends AbstractSchemaStructureViewElement<SchemaEntity> {
+
+    private static final Icon[] ICONS = {
+            SchemaIcons.Nodes.Entity, SchemaIcons.Nodes.Property, SchemaIcons.Nodes.SubProperty,
+    };
 
     public SchemaEntityStructureViewElement(SchemaEntity element) {
         super(element);
@@ -24,19 +26,26 @@ public class SchemaEntityStructureViewElement extends AbstractSchemaStructureVie
 
     @Override
     protected PresentationData createPresentation(SchemaEntity element) {
-        SchemaEntityHead head = element.getEntityHead();
+        SchemaBasicStructureDeclaration declaration = element.getEntityHead().getBasicStructureDeclaration();
         return new PresentationData(
-                head.getBasicStructureDeclaration().getStructureNameDeclaration().getText(),
-                head.getBasicStructureDeclaration().getStructureAliasDeclaration().getText(),
-                SchemaIcons.Nodes.Entity,
+                declaration.getStructureNameDeclaration().getText(),
+                declaration.getStructureAliasDeclaration().getText(),
+                this.getIcon(element),
                 null
         );
     }
 
     @Override
     public TreeElement @NotNull [] getChildren() {
-        List<SchemaProperty> elements = PsiTreeUtil.getChildrenOfTypeAsList(myElement, SchemaProperty.class);
-        return this.buildPropertyTreeElements(elements);
+        if (myElement.getEntityBody() == null) {
+            return EMPTY_ARRAY;
+        }
+
+        return this.buildPropertyTreeElements(myElement.getEntityBody().getPropertyList());
     }
 
+    private Icon getIcon(SchemaEntity element) {
+        int level = Math.max(0, Math.min(element.getLevel(), ICONS.length - 1));
+        return ICONS[level];
+    }
 }
--- idea/spg-schema-idea-plugin/src/main/java/org/openspg/idea/schema/structureView/viewElement/SchemaPropertyStructureViewElement.java
+++ idea/spg-schema-idea-plugin/src/main/java/org/openspg/idea/schema/structureView/viewElement/SchemaPropertyStructureViewElement.java
@@ -2,16 +2,18 @@ package org.openspg.idea.schema.structureView.viewElement;
 
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.util.treeView.smartTree.TreeElement;
-import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.openspg.idea.schema.SchemaIcons;
-import org.openspg.idea.schema.psi.SchemaEntity;
+import org.openspg.idea.schema.psi.SchemaBasicPropertyDeclaration;
 import org.openspg.idea.schema.psi.SchemaProperty;
-import org.openspg.idea.schema.psi.SchemaPropertyHead;
 
-import java.util.List;
+import javax.swing.*;
 
 public class SchemaPropertyStructureViewElement extends AbstractSchemaStructureViewElement<SchemaProperty> {
+
+    private static final Icon[] ICONS = {
+            SchemaIcons.Nodes.EntityMeta, SchemaIcons.Nodes.PropertyMeta, SchemaIcons.Nodes.SubPropertyMeta,
+    };
 
     public SchemaPropertyStructureViewElement(SchemaProperty element) {
         super(element);
@@ -24,19 +26,26 @@ public class SchemaPropertyStructureViewElement extends AbstractSchemaStructureV
 
     @Override
     protected PresentationData createPresentation(SchemaProperty element) {
-        SchemaPropertyHead head = element.getPropertyHead();
+        SchemaBasicPropertyDeclaration declaration = element.getPropertyHead().getBasicPropertyDeclaration();
         return new PresentationData(
-                head.getBasicPropertyDeclaration().getText(),
-                head.getBasicPropertyDeclaration().getText(),
-                SchemaIcons.Nodes.Entity,
+                declaration.getText(),
+                declaration.getText(),
+                this.getIcon(element),
                 null
         );
     }
 
     @Override
     public TreeElement @NotNull [] getChildren() {
-        List<SchemaEntity> elements = PsiTreeUtil.getChildrenOfTypeAsList(myElement, SchemaEntity.class);
-        return this.buildEntityTreeElements(elements);
+        if (myElement.getPropertyBody() == null) {
+            return EMPTY_ARRAY;
+        }
+
+        return this.buildEntityTreeElements(myElement.getPropertyBody().getEntityList());
     }
 
+    private Icon getIcon(SchemaProperty element) {
+        int level = Math.max(0, Math.min(element.getLevel() - 1, ICONS.length - 1));
+        return ICONS[level];
+    }
 }
